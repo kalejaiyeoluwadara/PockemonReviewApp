a33389d6bac99066880053f831d50ce36769fe91
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-04-09 00:11:56 +0100

    Remove obsolete database files and add Review entity and repository to the application. Update Program.cs for dependency injection and modify MappingProfiles to include Review mapping.

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using AutoMapper;
+using PokemonReviewApp.Dto;
+
+namespace PokemonReview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : Controller
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewController(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _reviewRepository = reviewRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _reviewRepository.GetReviews();
+            var reviewsDto = _mapper.Map<List<ReviewDto>>(reviews);
+            return Ok(reviewsDto);
+        }
+
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+            var review = _reviewRepository.GetReview(reviewId);
+            var reviewDto = _mapper.Map<ReviewDto>(review);
+            return Ok(reviewDto);
+        }
+
+        [HttpGet("{reviewId}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+            var reviews = _reviewRepository.GetReviewsOfAPokemon(reviewId);
+            return Ok(reviews);
+        }
+    }
+}
+
+        
+        
+ 
---
+++ Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int id);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExists(int id);
+    }
+}
